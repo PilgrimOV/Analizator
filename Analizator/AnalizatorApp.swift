64bf21aef2039d279a65401c7cb707b2d817b79b
Victor <pilgrimov@icloud.com>
2025-08-07 14:25:38 +0500

    Initial Commit

---
+++ Analizator/AnalizatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnalizatorApp.swift
+//  Analizator
+//
+//  Created by Виктор Обухов on 07.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct AnalizatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
